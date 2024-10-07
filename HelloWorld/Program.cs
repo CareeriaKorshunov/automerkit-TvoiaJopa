52532eda42b4746947e5f96cb39d5e6d0f5d3b73
Aretemii Korshunov <56575740+TvoiaJopa@users.noreply.github.com>
2024-10-07 08:50:51 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,26 @@
         {
             //Koodi tähän
           
+  List<string> autolista = new List<string>();
+            int kierrokset = 1;
 
+            Console.WriteLine("Lisää listalle viisi automerkkiä");
+
+            while (true)
+            {
+                autolista.Add(Console.ReadLine());
+                if (kierrokset == 5)
+                {
+                    break;
+                }
+                kierrokset++;
+            }
+            Console.WriteLine("\nsyötit listalle 5 merkkiä, tässä lista:");
+            Console.WriteLine(autolista[0]);
+            Console.WriteLine(autolista[1]);
+            Console.WriteLine(autolista[2]);
+            Console.WriteLine(autolista[3]);
+            Console.WriteLine(autolista[4]);
         }
     }
 }
